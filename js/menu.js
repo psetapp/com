e391419af86f387830c67e41534c791c1ab624a1
Alejandro Fernández Pérez <alex.audiovisual.engineer@gmail.com>
2023-09-21 23:45:52 +0200

    51º commit

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+const sidebarCollapseButton = document.getElementById('sidebarCollapse');
+const indice = document.getElementById('indice');
+
+sidebarCollapseButton.addEventListener('click', () => {
+    // Cuando se hace clic en el botón, cambia el estado del menú
+    if (indice.style.display === 'none' || indice.style.display === '') {
+        indice.style.display = 'block'; // Muestra el menú
+    } else {
+        indice.style.display = 'none'; // Oculta el menú
+    }
+});
