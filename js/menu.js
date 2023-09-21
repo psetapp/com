0aeded26aee9411c1ae16cf2f720675aea0764db
Alejandro Fernández Pérez <alex.audiovisual.engineer@gmail.com>
2023-09-22 00:31:17 +0200

    54º commit

--- js/menu.js
+++ js/menu.js
@@ -9,3 +9,12 @@ sidebarCollapseButton.addEventListener('click', () => {
         indice.style.display = 'none'; // Oculta el menú
     }
 });
+
+sidebarCollapseButton.addEventListener('touchstart', () => {
+    // Cuando se hace clic en el botón, cambia el estado del menú
+    if (indice.style.display === 'none' || indice.style.display === '') {
+        indice.style.display = 'block'; // Muestra el menú
+    } else {
+        indice.style.display = 'none'; // Oculta el menú
+    }
+});
